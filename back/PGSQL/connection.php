15693314ee197a4bf460812d8cb8fc8bf107d83e
Pepito Mercurio <96176596+PepitoMercurio@users.noreply.github.com>
2024-06-12 23:23:22 +0200

    Update connection.php

--- back/PGSQL/connection.php
+++ back/PGSQL/connection.php
@@ -3,11 +3,12 @@ header('Access-Control-Allow-Origin: http://localhost:3000');
 header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Content-Type');
 
-$host = '172.104.240.106';
+//host & password ont été enlevé pour un affichage public du repo
+$host = null;
 $port = '5432';
 $dbname = 'zindar';
 $username = 'zindar';
-$password = '@Ryan20072003@';
+$password = null;
 
 $connection = pg_connect("host=$host port=$port dbname=$dbname user=$username password=$password");
 if($connection){
